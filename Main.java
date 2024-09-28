747d6375b3248c6e6a408b65dde188d3d28eb237
joana <joanaalvesm@gmail.com>
2024-09-28 19:55:08 -0300

    feat: Login de funcionário.

--- Main.java
+++ Main.java
@@ -1,64 +1,100 @@
 import java.util.Scanner;
-import java.util.Random;
 
 public class Main {
     private static Scanner scanner = new Scanner(System.in);
     private static AgenciaDeViagem agenciaCentral = new AgenciaDeViagem();
+    private static Funcionario funcionarioLogado = null;
 
     public static void main(String[] args) {
-
-        System.out.println("Olá! Bem vindo a agencia de viagens :)");
+        System.out.println("Olá! Bem vindo à agência de viagens :)");
 
         while (true) {
-
-            menuPrincipal();
+            if (funcionarioLogado == null) {
+                menuInicial();
+            } else {
+                menuPrincipal();
+            }
 
             int escolha = scanner.nextInt();
             scanner.nextLine();
 
-            switch (escolha) {
-                case 1:
-                    System.out.println("-------- Busca de Passagens --------");
-                    imprimirDivisoriaComQuebraDeLinha();
-                    break;
-
-                case 2:
-                    cadastradoDeFuncionario();
-                    break;
-
-                case 3:
-                    cadastroDeCompanhiaAerea();
-                    break;
-
-                case 4:
-                    cadastroDeAeroporto();
-                    break;
-
-                case 5:
-                    System.out.println("Saindo...");
-                    scanner.close();
-                    return;
-
-                default:
-                    System.out.println("Opção inválida. Tente novamente.");
-                    break;
+            if (funcionarioLogado == null) {
+                switch (escolha) {
+                    case 1:
+                        cadastradoDeFuncionario();
+                        break;
+                    case 2:
+                        loginFuncionario();
+                        break;
+                    default:
+                        System.out.println("Opção inválida. Tente novamente.");
+                        break;
+                }
+            } else {
+                switch (escolha) {
+                    case 1:
+                        System.out.println("-------- Busca de Passagens --------");
+                        Utils.imprimirDivisoriaComQuebraDeLinha();
+                        break;
+                    case 2:
+                        cadastradoDeFuncionario();
+                        break;
+                    case 3:
+                        cadastroDeCompanhiaAerea();
+                        break;
+                    case 4:
+                        cadastroDeAeroporto();
+                        break;
+                    case 5:
+                        System.out.println("Saindo...");
+                        scanner.close();
+                        return;
+                    case 6:
+                        funcionarioLogado = null;
+                        System.out.println("Logout realizado com sucesso.");
+                        break;
+                    default:
+                        System.out.println("Opção inválida. Tente novamente.");
+                        break;
+                }
             }
         }
     }
-
-    public static void imprimirDivisoriaComQuebraDeLinha() {
-        System.out.println("-------------------------------------------\n");
+ 
+    public static void menuInicial() {
+        System.out.println("1. Cadastrar Funcionário");
+        System.out.println("2. Fazer Login");
     }
 
     public static void menuPrincipal() {
-        System.out.println("--------------------------------------");
-        System.out.println("O que deseja fazer?");
-        // System.out.println("1- Buscar passagens");
-        System.out.println("2- Cadastrar funcionario");
-        System.out.println("3- Cadastrar companhia aerea");
-        System.out.println("4- Cadastrar aeroporto");
-        System.out.println("5- SAIR");
-        System.out.println("");
+        System.out.println("1. Busca de Passagens");
+        System.out.println("2. Cadastro de Funcionário");
+        System.out.println("3. Cadastro de Companhia Aérea");
+        System.out.println("4. Cadastro de Aeroporto");
+        System.out.println("5. Sair");
+        System.out.println("6. Logout");
+    }
+
+    public static void loginFuncionario() {
+        System.out.println("-------- Login de Funcionário --------");
+
+        System.out.println("Digite o CPF: ");
+        String cpf = scanner.nextLine();
+
+        System.out.println("Digite a senha: ");
+        String senha = scanner.nextLine();
+
+        for (Funcionario funcionario : agenciaCentral.getFuncionarios()) {
+            if (funcionario.getCpf().equals(cpf) && funcionario.getSenha().equals(senha)) {
+                funcionarioLogado = funcionario;
+                System.out.println("\nLogin realizado com sucesso. Bem-vindo, " + funcionario.getNome() + "!");
+                Utils.imprimirDivisoriaComQuebraDeLinha();
+                return;
+            }
+        }
+
+        System.out.println("CPF ou senha inválidos. Tente novamente.");
+        Utils.imprimirDivisoriaComQuebraDeLinha();
     }
 
     public static void cadastradoDeFuncionario() {
@@ -68,7 +104,7 @@ public class Main {
         while (true) {
             System.out.println("Digite o nome do funcionário: ");
             nome = scanner.nextLine();
-            if (validarString(nome, 0)) {
+            if (Utils.validarString(nome, 0)) {
                 break;
             }
             System.out.println("Nome inválido. Tente novamente.");
@@ -88,7 +124,7 @@ public class Main {
         while (true) {
             System.out.println("Digite o Email: ");
             email = scanner.nextLine();
-            if (validarString(email, 4) && email.contains("@")) {
+            if (Utils.validarString(email, 3) && email.contains("@")) {
                 break;
             }
             System.out.println("Email inválido. Tente novamente.");
@@ -98,7 +134,7 @@ public class Main {
         while (true) {
             System.out.println("Digite a senha: ");
             senha = scanner.nextLine();
-            if (validarString(senha, 6)) {
+            if (Utils.validarString(senha, 7)) {
                 break;
             }
             System.out.println("Senha inválida. Tente novamente.");
@@ -107,11 +143,7 @@ public class Main {
         Funcionario funcionario = new Funcionario(nome, cpf, email, senha);
         agenciaCentral.adicionarFuncionario(funcionario);
         System.out.println("Funcionario " + funcionario.getNome() + " cadastrado com sucesso");
-        imprimirDivisoriaComQuebraDeLinha();
-    }
-
-    public static boolean validarString(String str, int tamanhoMinimo) {
-        return !str.isEmpty() && str.length() > tamanhoMinimo;
+        Utils.imprimirDivisoriaComQuebraDeLinha();
     }
 
     public static void cadastroDeCompanhiaAerea() {
@@ -121,7 +153,7 @@ public class Main {
         while (true) {
             System.out.println("Digite o nome da companhia: ");
             nomeCiaAerea = scanner.nextLine();
-            if (validarString(nomeCiaAerea, 3)) {
+            if (Utils.validarString(nomeCiaAerea, 3)) {
                 break;
             }
             System.out.println("Nome inválido. Tente novamente.");
@@ -131,7 +163,7 @@ public class Main {
         while (true) {
             System.out.println("Digite a razao social: ");
             rSocial = scanner.nextLine();
-            if (validarString(rSocial, 3)) {
+            if (Utils.validarString(rSocial, 3)) {
                 break;
             }
             System.out.println("Razão social inválida. Tente novamente.");
@@ -141,22 +173,17 @@ public class Main {
         while (true) {
             System.out.println("Digite o CNPJ: ");
             cnpj = scanner.nextLine();
-            if (validarString(cnpj, 3)) {
+            if (Utils.validarString(cnpj, 3)) {
                 break;
             }
             System.out.println("CNPJ inválido. Tente novamente.");
         }
 
-        int codigoCiaAerea = gerarCodigoCiaAerea();
+        int codigoCiaAerea = Utils.gerarCodigoDeID();
         CompanhiaAerea companhia = new CompanhiaAerea(codigoCiaAerea, nomeCiaAerea, rSocial, cnpj);
         agenciaCentral.adicionarCompanhiaAerea(companhia);
         System.out.println("Companhia Aérea " + companhia.getNome() + " cadastrada com sucesso");
-        imprimirDivisoriaComQuebraDeLinha();
-    }
-
-    public static int gerarCodigoCiaAerea() {
-        Random random = new Random();
-        return 10 + random.nextInt(90);
+        Utils.imprimirDivisoriaComQuebraDeLinha();
     }
 
     public static void cadastroDeAeroporto() {
@@ -178,6 +205,6 @@ public class Main {
 
         Aeroporto aerop = new Aeroporto(nome, sigla, cidade, estado, pais);
         System.out.println("Aeroporto " + aerop.getNome() + " cadastrado com sucesso");
-        imprimirDivisoriaComQuebraDeLinha();
-    }
+        Utils.imprimirDivisoriaComQuebraDeLinha();
+        }
 }
---
+++ Utils.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class Utils {
+
+    public static boolean validarString(String str, int tamanhoMinimo) {
+        return !str.isEmpty() && str.length() > tamanhoMinimo;
+    }
+
+    public static void imprimirDivisoriaComQuebraDeLinha() {
+        System.out.println("-------------------------------------------\n");
+    }
+
+    public static int gerarCodigoDeID() {
+        Random random = new Random();
+        return 10 + random.nextInt(90);
+    }
+}
