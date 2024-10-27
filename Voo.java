b298e7bd9496516342eae0ec504cb25cc143ca73
Julia Vianna <julia.vvianna@gmail.com>
2024-10-27 14:27:25 -0300

    feat (2.3): inativar voo 30 dias após a instanciação

--- Voo.java
+++ Voo.java
@@ -1,4 +1,7 @@
+import java.time.LocalDate;
 import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Voo  {
     String origem;
@@ -9,7 +12,35 @@ public class Voo  {
     private double valorBasico;
     private double valorBusiness;
     private double valorPremium;
+    private boolean ativo = true;
+    private LocalDate dataCriacao;
+    private LocalDate dataLimite;
+    private List<Voo> voos = new ArrayList<>();
+    
 
+    public Voo(String origem, String destino, Date dtHr, String codigo, CompanhiaAerea companhia, double valorBasico, double valorBusiness, double valorPremium) {
+        this.origem = origem;
+        this.destino = destino;
+        this.dtHr = dtHr;
+        this.codigo = codigo;
+        this.companhia = companhia;
+        this.valorBasico = valorBasico;
+        this.valorBusiness = valorBusiness;
+        this.valorPremium = valorPremium;
+
+        this.dataCriacao = LocalDate.now();
+        this.dataLimite = dataCriacao.plusDays(30);     
+
+        voos.add(this);
+    }
+
+    public void adicionarVoo(Voo voo) {
+        this.voos.add(voo);
+    }
+    
+    public List<Voo> getVoos() {
+        return voos;
+    }
     public Date getDataHora() {
         return dtHr;
     }
@@ -65,6 +96,12 @@ public class Voo  {
         
     }
 */
+    public void inativarVoo(Voo voo) {
+        LocalDate dataAtual = LocalDate.now();
+        if (dataAtual.isAfter(voo.dataLimite)) {
+            ativo = false;
+        }
+    }
 
 
     public String getInfosVoo() {
